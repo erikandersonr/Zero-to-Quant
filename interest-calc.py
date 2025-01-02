2adf04360a8eaee497e5576cc5830b34500b8adf
Erik Anderson <erikandersonr@outlook.com>
2025-01-02 15:32:52 -0500

    Final interest rate calculator

---
+++ interest-calc.py
@@ -0,0 +1,7 @@
+p = float(input("Enter the principal amount: "))
+r = float(input("Enter the interest rate: "))
+t = float(input("Enter the time period: "))
+
+calculated_interest = ((p * r * t)/100)
+
+print("The calculated interest is ", calculated_interest)
